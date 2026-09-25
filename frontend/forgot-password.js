a5cada061e5bff23a0523830045957eaa6b5fa91
rajarshi-dutta <rajarshidutta18@gmail.com>
2026-09-25 17:09:48 +0530

    correcting fetch id

--- frontend/forgot-password.js
+++ frontend/forgot-password.js
@@ -230,7 +230,7 @@ verifyOtpBtn.addEventListener(
         try {
 
             const response = await fetch(
-                "http://127.0.0.1:5000/api/auth/verify-otp",
+                "http://127.0.0.1:5050/api/auth/verify-otp",
                 {
                     method: "POST",
 
