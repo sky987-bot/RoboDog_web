ce01b2fe797d347ab2d21eef3dc1f917c44e031f
rajarshi-dutta <rajarshidutta18@gmail.com>
2026-09-25 15:41:50 +0530

    Deploying the database

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
+import os
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 
 client = MongoClient(MONGO_URL)
 
@@ -8,6 +9,4 @@ db = client["robodog"]
 
 users_collection = db["users"]
 alerts_collection = db["alerts"]
-password_resets_collection = db["password_resets"]
-
 print("MongoDB Connected Successfully!")
--- frontend/script.js
+++ frontend/script.js
@@ -13,7 +13,7 @@ registerForm.addEventListener("submit", async function (event) {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/api/auth/register",
+            "http://127.0.0.1:5050/api/auth/register",
             {
                 method: "POST",
 
